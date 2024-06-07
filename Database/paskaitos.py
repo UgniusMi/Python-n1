a183e925a889d1b66c50fb9cb29c1ca54f9d52bd
UgniusMi <u.miliauskas@gmail.com>
2024-06-07 11:45:15 +0300

    duonbaze

--- Database/paskaitos.py
+++ Database/paskaitos.py
@@ -8,18 +8,20 @@ c = conn.cursor()
 with conn:
     c.execute("""CREATE TABLE IF NOT EXISTS
     paskaita (  
+    ID integer PRIMARY KEY AUTOINCREMENT,
     PAVADINIMAS text,
     DESTYTOJAS text,
     TRUKME INTEGER
     )""")
 
+    c.execute("DELETE FROM paskaita") #kiekviena karta paleidziant issitrinam (jeigu norim)
 
 #Paskaitu pridejimas
 
 with conn:
-    c.execute("INSERT INTO paskaita VALUES ('Vadyba', 'Domantas', 40)")
-    c.execute("INSERT INTO paskaita VALUES ('Python', 'Donatas', 80)")
-    c.execute("INSERT INTO paskaita VALUES ('Java', 'Tomas', 80)")
+    c.execute("INSERT INTO paskaita (PAVADINIMAS, DESTYTOJAS, TRUKME) VALUES ('Vadyba', 'Domantas', 40)") #JEI NAUDOJAM ID PRIMARY KEY, IKELDAMI NAUJA IRASA NURODOM KOKIU VALUES TIKIMES
+    c.execute("INSERT INTO paskaita (PAVADINIMAS, DESTYTOJAS, TRUKME) VALUES ('Python', 'Donatas', 80)")
+    c.execute("INSERT INTO paskaita (PAVADINIMAS, DESTYTOJAS, TRUKME) VALUES ('Java', 'Tomas', 80)")
 
 # Tukme ilgesne nei 50
 
@@ -30,19 +32,21 @@ with conn:
     print(f"\nPaskaitos kurios vyksta ilgiau nei 50min: {paskaitos}")
     
 
-# pakeiciau paskaitos pavadinima 
+# # pakeiciau paskaitos pavadinima 
 
 with conn:
     c.execute("UPDATE paskaita SET PAVADINIMAS = 'Python programavimas' WHERE PAVADINIMAS = 'Python'")
-
-# trinam toma
+    
+# # trinam toma
 
 with conn:
     c.execute ("DELETE FROM paskaita WHERE DESTYTOJAS = 'Tomas'")
     c.execute("SELECT * FROM paskaita")
-    print(c.fetchall())
+    tomless = c.fetchall()
+    for paskait in tomless:
+        print(f"\nLike paskaitos pasalinus Toma: {paskait}")
 
-#spausdinam
+# #spausdinam
 with conn:
     c.execute("SELECT * FROM paskaita")
     result = c.fetchall()
